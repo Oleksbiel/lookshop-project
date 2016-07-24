1888a96e63638b32ce6aa4f46018abcc931ae0b6
Kate Kopchuk <katykopchuk0407@gmail.com>
2016-07-24 13:38:52 +0300

    promo-home

---
+++ markup/js/assets.js
@@ -0,0 +1,12 @@
+$('.carousel').owlCarousel({
+    autoPlay: 4000,
+    navigation: true,
+    singleItem:true,
+    navigationText : ["",""]
+})
+$('.carousel-2').owlCarousel({
+    autoPlay: 4000,
+    navigation: true,
+    singleItem:true,
+    navigationText : ["",""]
+})
